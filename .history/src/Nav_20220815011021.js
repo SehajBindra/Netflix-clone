72b5e791557fa5ec0f0f2e79bf3e9c88ceeb6f81
Sehaj bindra <sehajbindra1234@gmail.com>
2022-08-15 01:10:31 +0530

    Nav

--- .history/src/Nav_20220815011021.js
+++ .history/src/Nav_20220815011021.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import "./Nav.css";
 function Nav() {
   const [show, handleShow] = useState(false);
+  const history = useHistory();
 
   const transitionNavbar = () => {
-    if (Window.scrollY > 100) {
+    if (window.scrollY > 100) {
       handleShow(true);
     } else {
       handleShow(false);
@@ -20,12 +22,14 @@ function Nav() {
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
+          onClick={() => history.push("/")}
           className="nav_logo"
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          // src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
 
         <img
+          onClick={() => history.push("/profile")}
           className="nav_Avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt=""
